74f2f017663ffee8f9b44bbca7dc8fe02807f60d
Joe <wjs5337@nyu.edu>
2026-06-13 01:01:46 -0400

    First running version - prints incoming variable "message" (GET command).

--- webServer.py
+++ webServer.py
@@ -12,17 +12,37 @@ def webServer(port=13331):
   serverSocket.bind(("", port))
   
   #Fill in start
-
+  serverSocket.listen(1)
   #Fill in end
+
+  connectionSocket, addr = serverSocket.accept()
+
+  message = connectionSocket.recv(1024)
+
+  print(f"message: {message}")
+
+  connectionSocket.close()  # closing the connection socket
+
+  """
 
   while True:
     #Establish the connection
-    
+
     print('Ready to serve...')
-    connectionSocket, addr = #Fill in start -are you accepting connections?     #Fill in end
-    
+    # connectionSocket, addr = #Fill in start -are you accepting connections?     #Fill in end
+    connectionSocket, addr = serverSocket.accept()
+
+    message = connectionSocket.recv(1024)
+
+
+
     try:
-      message = #Fill in start -a client is sending you a message   #Fill in end 
+      # message = #Fill in start -a client is sending you a message   #Fill in end
+      message = connectionSocket.recv(1024)
+      
+      print(message)
+
+
       filename = message.split()[1]
       
       #opens the client requested file. 
@@ -52,7 +72,6 @@ def webServer(port=13331):
 
 
       # Fill in end
-        
       connectionSocket.close() #closing the connection socket
       
     except Exception as e:
@@ -72,6 +91,7 @@ def webServer(port=13331):
   # DO NOT PLACE ANYWHERE ELSE AND DO NOT UNCOMMENT WHEN SUBMITTING, YOU ARE GONNA HAVE A BAD TIME
   #serverSocket.close()
   #sys.exit()  # Terminate the program after sending the corresponding data
+"""
 
 if __name__ == "__main__":
   webServer(13331)
